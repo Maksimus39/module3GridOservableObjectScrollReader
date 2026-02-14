b6f38d7d938bb362fc8f69eb9161d501a6effa45
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-14 13:55:13 +0300

    Initial Commit

---
+++ module3GridOservableObjectScrollReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  module3GridOservableObjectScrollReader
+//
+//  Created by Максим Минаков on 14.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ module3GridOservableObjectScrollReader/module3GridOservableObjectScrollReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3GridOservableObjectScrollReaderApp.swift
+//  module3GridOservableObjectScrollReader
+//
+//  Created by Максим Минаков on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3GridOservableObjectScrollReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
