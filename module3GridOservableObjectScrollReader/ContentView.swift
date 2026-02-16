0794d9a2da826415081cf9166bbcbd0eade36764
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-16 10:32:43 +0300

    grid

--- module3GridOservableObjectScrollReader/ContentView.swift
+++ module3GridOservableObjectScrollReader/ContentView.swift
@@ -6,20 +6,22 @@ import SwiftUI
 
 struct ContentView: View {
     let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 10),
         GridItem(.flexible()),
-        GridItem(.flexible()),
-        //GridItem(.fixed(100))
+        //GridItem(.adaptive(minimum: 50, maximum: 100)),
     ]
     
     var body: some View {
         ScrollView (.vertical, showsIndicators: true) {
-            LazyVGrid(columns: columns) {
+            LazyVGrid(columns: columns, spacing: 40) {
                 ForEach(0...9200000, id: \.self) { index in
                     Rectangle()
                         .fill(.cyan)
-                        .frame(height: 50)
+                        .frame(height: 200)
                         .overlay {
                             Text("index: \(index)")
+                                .foregroundStyle(.black)
+                                .font(.system(size: 20))
                         }
                 }
             }
